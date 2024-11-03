c14afb07ef961d3ec4768f8338b7fa4898b0030c
Sepide-cmd <mojala15@rowan.edu>
2024-11-03 17:23:12 -0500

    Add files via upload

---
+++ src/server.py
@@ -0,0 +1,24 @@
+import multiprocessing
+multiprocessing.set_start_method('spawn')
+
+# server.py
+import flwr as fl
+from model.lstm_model import LSTMModel  # Import the LSTM model
+
+def start_federated_learning():
+    """Start the Federated Learning server."""
+    model = LSTMModel()
+
+    # Start the Flower server
+    fl.server.start_server(
+        server_address="0.0.0.0:5000",
+        config=fl.server.ServerConfig(num_rounds=5),
+        strategy=fl.server.strategy.FedAvg(
+            min_fit_clients=2,
+            min_available_clients=2,
+        )
+    )
+
+if __name__ == "__main__":
+    print("Starting Flower server...")
+    start_federated_learning()
